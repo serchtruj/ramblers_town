76d10257dfc34d6a883460bad3411d07017c2021
serchtruj <serchtruj1@gmail.com>
2023-08-02 05:15:50 -0500

    Add base footer

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Navigation from './Nav/Navigation'
+import SocialMedia from './Social Media/SocialMedia'
+import News from './News/News'
+import Footer from './Footer/Footer'
+import ImageBgSection from './Image Bg Section/ImageBgSection'
+
+export { Navigation, SocialMedia, News, Footer, ImageBgSection }
--- src/components/index.tsx
+++
@@ -1,5 +0,0 @@
-import Navigation from './Nav/Navigation'
-import SocialMedia from './Social Media/SocialMedia'
-import News from './News/News'
-
-export {Navigation, SocialMedia, News}
